03dba7a27f77f8d7b9ca6e0f2dcd7b8a9ced1299
Pankaj Tanwar <pankajtanwar@paisabazaar.com>
2021-10-25 01:22:57 +0530

    first commit :rocket:

---
+++ data/match.js
@@ -0,0 +1,19 @@
+const CRICBUZZ = {
+    MATCHES : {
+        "LIVE_MATCHES" : {
+            "name" : "Live Match",
+            "url" : "https://m.cricbuzz.com/cricket-match/live-scores/"
+        },
+        "RECENT_MATCHES" : {
+            "name" : "Recent Match",
+            "url" : "https://m.cricbuzz.com/cricket-match/live-scores/recent-matches"
+        },
+        "UPCOMING_MATCHES" : {
+            "name" : "Upcoming Match",
+            "url" : "https://m.cricbuzz.com/cricket-match/live-scores/upcoming-matches"
+        }
+    },
+    WEBSITE_URL : "https://m.cricbuzz.com"
+}
+
+module.exports = CRICBUZZ
